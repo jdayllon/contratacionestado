e97e9470de190cdc6bf8e56fe04da257db268244
jdayllon <jdayllon@gmail.com>
2013-11-13 07:25:17 +0100

    Primeros desarrollos
    Primeros desarrollos

--- estado/estado/spiders/adjudica.py
+++ estado/estado/spiders/adjudica.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 __author__ = 'jdayllon'
 
 from scrapy.spider import BaseSpider
@@ -6,11 +7,27 @@ class AdjudicaSpider(BaseSpider):
     name = "Adjudicaciones"
     allowed_domains = ["https://contrataciondelestado.es"]
     start_urls = [
-        "https://contrataciondelestado.es",
+        "https://contrataciondelestado.es/wps/portal/plataforma",
     ]
 
-    1ST_LINK = ""//a[@alt='Ver más']""
+    SEEMORE_LINK = '//a[@alt="See more"]/@href'
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        open(filename, 'wb').write(response.body)
+        #filename = response.url.split("/")[-2]
+        #open(filename, 'wb').write(response.body)
+
+        sel = Selector(response)
+        contracts = sel.xpath(self.SEEMORE_LINK)[1]
+        Request("https://contrataciondelestado.es%s" % contracts.extract(), callback=self.parse)
+
+
+    def parse_page_list(self, response):
+
+        sel = Selector(response)
+
+
+        #for site in sites:
+        #    title = site.xpath('a/text()').extract()
+        #    link = site.xpath('a/@href').extract()
+        #    desc = site.xpath('text()').extract()
+        #    print title, link, desc
