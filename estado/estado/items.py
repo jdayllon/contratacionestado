076c30ffd5d4540f17a536d2b1027ff64a340953
jdayllon <jdayllon@gmail.com>
2013-11-15 07:25:05 +0100

    Primera Extracción
    Se obtiene la primera extracción de datos del contratacióndelestado.es

--- estado/estado/items.py
+++ estado/estado/items.py
@@ -5,7 +5,7 @@
 
 from scrapy.item import Item, Field
 
-class EstadoItem(Item):
+class FileItem(Item):
     # define the fields for your item here like:
     # name = Field()
     expediente = Field()
